0e82dd0d5a4a565d7f56212ef0d19effb8cc6d5e
lybxlpsv <lybxlpsv@users.noreply.github.com>
2017-03-31 15:07:50 +0700

    Update Program_355_82.cs

--- Program_355_82.cs
+++ Program_355_82.cs
@@ -1,3 +1,4 @@
+using GpuzDemo;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,7 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GpuzDemo
+namespace nvavoidthrottling
 {
     class Program
     {
@@ -15,86 +16,124 @@ namespace GpuzDemo
         {
             public bool stahp = false;
             public int throttletemp_0 = 1;
-            public int nothrottlestate = 0;
-            public int throttletemp_1 = 55;
-            public int throttlestate_1 = -25;
-            public int throttletemp_2 = 56;
-            public int throttlestate_2 = -50;
-            public int throttletemp_3 = 57;
-            public int throttlestate_3 = -75;
-            public int throttletemp_4 = 58;
-            public int throttlestate_4 = -80;
-            public int throttletemp_5 = 59;
-            public int throttlestate_5 = -100;
-            public int throttletemp_6 = 60;
-            public int throttlestate_6 = -100;
-            public int throttletemp_7 = 61;
-            public int throttlestate_7 = -100;
-            public int throttletemp_8 = 62;
-            public int throttlestate_8 = -100;
-            public int throttletemp_9 = 63;
-            public int throttlestate_9 = -100;
-            public int throttletemp_10 = 64;
-            public int throttlestate_10 = -100;
-            public int proposedstate = 0;
-            public int lastproposedstate = 0;
+            public int nothrottlestate = 1063;
+            public int throttletemp_1 = 57;
+            public int throttlestate_1 = 1023;
+            public int throttletemp_2 = 58;
+            public int throttlestate_2 = 1010;
+            public int throttletemp_3 = 59;
+            public int throttlestate_3 = 997;
+            public int throttletemp_4 = 60;
+            public int throttlestate_4 = 984;
+            public int throttletemp_5 = 61;
+            public int throttlestate_5 = 971;
+            public int memorystate = 1149;
+            public int proposedstate = 960;
+            public int lastproposedstate = 960;
             public bool clockhaschanged = true;
+            public bool lastgpuusage = false;
+            public bool gpuusage = false;
             public int temp = 0;
+            public int usage = 0;
             public void check()
             {
                 while (stahp == false)
                 {
-
                     Console.Clear();
                     Console.SetCursorPosition(0, 0);
                     GpuzWrapper gpuz = new GpuzWrapper();
                     gpuz.Open();
                     temp = (int)Math.Round(gpuz.SensorValue(2));
-                    Console.WriteLine("Factory GPU Clock" + ": " + gpuz.DataValue(22) + "Mhz");
+                    usage = (int)Math.Round(gpuz.SensorValue(3));
+                    Console.WriteLine("== lybAdaptiveOC ==\nfor DELL GM107-A, SK Hynix, Target Temps 53-57\n");
+                    Console.WriteLine("Factory GPU Clock" + ": 876 Mhz"); //from notebookcheck
                     Console.WriteLine(gpuz.SensorName(0) + ": " + gpuz.SensorValue(0) + " " + gpuz.SensorUnit(0));
-                    Console.WriteLine(gpuz.SensorName(1) + ": " + gpuz.SensorValue(1) + " " + gpuz.SensorUnit(1));
-                    Console.WriteLine(gpuz.SensorName(4) + ": " + gpuz.SensorValue(4) + " " + gpuz.SensorUnit(4));
+                    Console.WriteLine(gpuz.SensorName(2) + ": " + gpuz.SensorValue(2) + " " + gpuz.SensorUnit(2));
+                    Console.WriteLine(gpuz.SensorName(3) + ": " + gpuz.SensorValue(3) + " " + gpuz.SensorUnit(3));
                     Console.WriteLine(gpuz.SensorName(10) + ": " + gpuz.SensorValue(10) + " " + gpuz.SensorUnit(10));
-                    if ((temp >= throttletemp_0) && (temp < throttletemp_1)) { proposedstate = nothrottlestate; Console.WriteLine("NO THROTTLING"); }
-                    if ((temp >= throttletemp_1) && (temp < throttletemp_2)) { proposedstate = throttlestate_1; Console.WriteLine("OC STATE 1"); }
-                    if ((temp >= throttletemp_2) && (temp < throttletemp_3)) { proposedstate = throttlestate_2; Console.WriteLine("OC STATE 2"); }
-                    if ((temp >= throttletemp_3) && (temp < throttletemp_4)) { proposedstate = throttlestate_3; Console.WriteLine("OC STATE 3"); }
-                    if ((temp >= throttletemp_4) && (temp < throttletemp_5)) { proposedstate = throttlestate_4; Console.WriteLine("OC STATE 4"); }
-                    if ((temp >= throttletemp_5) && (temp < throttletemp_6)) { proposedstate = throttlestate_5; Console.WriteLine("OC STATE 5"); }
-                    if ((temp >= throttletemp_6) && (temp < throttletemp_7)) { proposedstate = throttlestate_6; Console.WriteLine("OC STATE 6"); }
-                    if ((temp >= throttletemp_7) && (temp < throttletemp_8)) { proposedstate = throttlestate_7; Console.WriteLine("OC STATE 7"); }
-                    if ((temp >= throttletemp_8) && (temp < throttletemp_9)) { proposedstate = throttlestate_8; Console.WriteLine("OC STATE 8"); }
-                    if ((temp >= throttletemp_9) && (temp < throttletemp_10)) { proposedstate = throttlestate_9; Console.WriteLine("OC STATE 9"); }
-                    if (temp >= throttletemp_10) { proposedstate = throttlestate_10; Console.WriteLine("OC STATE 10 --HOT!--"); }
+                    if ((temp >= throttletemp_0) && (temp < throttletemp_1)) { proposedstate = nothrottlestate; Console.WriteLine("OC STATE 5"); }
+                    if ((temp >= throttletemp_1) && (temp < throttletemp_2)) { proposedstate = throttlestate_1; Console.WriteLine("OC STATE 4"); }
+                    if ((temp >= throttletemp_2) && (temp < throttletemp_3)) { proposedstate = throttlestate_2; Console.WriteLine("OC STATE 3"); }
+                    if ((temp >= throttletemp_3) && (temp < throttletemp_4)) { proposedstate = throttlestate_3; Console.WriteLine("OC STATE 2"); }
+                    if ((temp >= throttletemp_4) && (temp < throttletemp_5)) { proposedstate = throttlestate_4; Console.WriteLine("OC STATE 1"); }
+                    if (temp >= throttletemp_5) { proposedstate = throttlestate_5; Console.WriteLine("OC STATE 0"); }
 
                     if (proposedstate != lastproposedstate) clockhaschanged = false;
 
-                    if (clockhaschanged == false)
+                    if ((clockhaschanged == false) && ((int)gpuz.SensorValue(0) != 0))
                     {
                         var p = new Process();
                         p.StartInfo.FileName = @"C:\Users\lybxl\Downloads\NV-Inspector-[Guru3D.com]\Guru3D.com\nvidiaInspector.exe";
-                        p.StartInfo.Arguments = "-setPstateLimit:0,0 -setMemoryClockOffset:0,0," + proposedstate;
+                        //TODO : PATH CONFIGURATION
+                        p.StartInfo.Arguments = "-setPstateLimit:0,5 -setGPUClock:0,1," + proposedstate;
                         p.Start();
                         clockhaschanged = true;
                         lastproposedstate = proposedstate;
                     }
+                    
+                    if (usage > 2) gpuusage = true; else gpuusage = false;
 
-                    Console.WriteLine("Proposed MEM CLOCK = " + (proposedstate + 900) + "Mhz, OC " + ((900 + proposedstate) - 900) + "Mhz");
-                    Console.WriteLine("Press ENTER to quit");
+                    //DELL FAN CONTROL
+                    if (lastgpuusage != gpuusage)
+                    {
+                        if (gpuusage == false)
+                        {
+                            var c = new Process();
+                            c.StartInfo.FileName = @"C:\Users\lybxl\Downloads\Compressed\RwPortableX64V1.6.9\Win64\Portable\Rw.exe";
+                            //TODO : PATH CONFIGURATION
+                            c.StartInfo.Arguments = "\"/command=WEC 96 64;RwExit;\"";
+                            c.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            c.StartInfo.UseShellExecute = false;
+                            c.StartInfo.RedirectStandardOutput = true;
+                            c.StartInfo.RedirectStandardError = true;
+                            //c.Start();
+                        }
+                        else
+                        {
+                            var d = new Process();
+                            d.StartInfo.FileName = @"C:\Users\lybxl\Downloads\Compressed\RwPortableX64V1.6.9\Win64\Portable\Rw.exe";
+                            //TODO : PATH CONFIGURATION
+                            d.StartInfo.Arguments = "\"/command=WEC 96 68;RwExit;\"";
+                            d.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                            d.StartInfo.UseShellExecute = false;
+                            d.StartInfo.RedirectStandardOutput = true;
+                            d.StartInfo.RedirectStandardError = true;
+                            //d.Start();
+
+                        }
+                        lastgpuusage = gpuusage;
+                    }
+
+                    Console.WriteLine("Target Core Clock   = " + proposedstate + "Mhz, OC +" + (proposedstate-876) + "Mhz, " + (int)(proposedstate * 640 *2/1000) + " Gflops");
+                    Console.WriteLine("Target Memory Clock = " + memorystate + "Mhz" + ", OC +" + (memorystate -800) + "Mhz" + ", " + (memorystate * 128 * 2 /8) + " Mb/s");
+                    Console.WriteLine("ALT+F4 to EXIT");
                     gpuz.Close();
-                    Thread.Sleep(500);
+                    Thread.Sleep(2000);
                 }
             }
 
         }
-
         static void Main(string[] args)
         {
+            //TODO : RUN AS ADMIN CHECK
+            //TODO : READ AND WRITE CONFIGURATION
             Alpha alp = new Alpha();
+            var u = new Process();
+            u.StartInfo.FileName = @"C:\Users\lybxl\Downloads\NV-Inspector-[Guru3D.com]\Guru3D.com\nvidiaInspector.exe";
+            //TODO : PATH CONFIGURATION
+            u.StartInfo.Arguments = "-setPstateLimit:0,0";
+            u.Start();
+            Thread.Sleep(500);
+            var n = new Process();
+            n.StartInfo.FileName = @"C:\Users\lybxl\Downloads\NV-Inspector-[Guru3D.com]\Guru3D.com\nvidiaInspector.exe";
+            //TODO : PATH CONFIGURATION
+            n.StartInfo.Arguments = "-setBaseClockOffset:0,0,135 -setMemoryClockOffset:0,0,249";
+            n.Start();
+            Thread.Sleep(500);
             var p = new Process();
             p.StartInfo.FileName = @"C:\Users\lybxl\Downloads\NV-Inspector-[Guru3D.com]\Guru3D.com\nvidiaInspector.exe";
             //TODO : PATH CONFIGURATION
-            p.StartInfo.Arguments = "-setPstateLimit:0,0";
+            p.StartInfo.Arguments = "-setPstateLimit:0,5 -setMemoryClock:0,1," + alp.memorystate;
             p.Start();
             Thread oThread = new Thread(new ThreadStart(alp.check));
             oThread.Start();
